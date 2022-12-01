09c4a34a2e7343c765fb67e8bf1c0ec2e0af6537
Wouter <wouter075@gmail.com>
2022-12-01 14:29:08 +0100

    Initial commit

---
+++ day1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = System.IO.File.ReadAllText(@"../../../day1.txt").Split("\n\n");
+            var calories = lines.Select(line => line.Split("\n").Select(int.Parse).ToList()).Select(l => l.Sum()).ToList().OrderByDescending(x => x);
+            
+            Console.WriteLine($"Part 1: {calories.Max()}");
+            Console.WriteLine($"Part 2: {calories.Take(3).Sum()}");
+        }
+    }
+}
